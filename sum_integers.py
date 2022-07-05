d1e3af2f4cb9a5714693834eb2d6cc8697accb8f
Jude Osborn <judeosborn@google.com>
2022-07-05 16:20:41 +1000

    sum_integers added

---
+++ sum_integers.py
@@ -0,0 +1,7 @@
+# Challenge: Sum two ingegers
+# Create a function called sum_integers that accepts two integers and returns their sum.
+
+# Tests:
+assert sum_integers(10, 1) == 11
+assert sum_integers(400, 1000) == 1400
+assert sum_integers(0, 0) == 0
